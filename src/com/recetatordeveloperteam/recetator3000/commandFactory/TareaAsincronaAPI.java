16da71feacafcc46d2cbbc0dd5442779875c1df1
Plandevida <plandevida@planes.com>
2014-05-19 22:07:06 +0200

    Montando la lista de recetas

--- src/com/recetatordeveloperteam/recetator3000/commandFactory/TareaAsincronaAPI.java
+++ src/com/recetatordeveloperteam/recetator3000/commandFactory/TareaAsincronaAPI.java
@@ -15,6 +15,7 @@ import org.json.JSONObject;
 import android.content.Context;
 import android.support.v4.content.AsyncTaskLoader;
 
+import com.recetatordeveloperteam.recetator3000.Receta;
 import com.recetatordeveloperteam.recetator3000.commandFactory.eventos.IdEvent;
 import com.recetatordeveloperteam.recetator3000.commandFactory.httpconexion.HttpHelper;
 
@@ -22,10 +23,11 @@ import com.recetatordeveloperteam.recetator3000.commandFactory.httpconexion.Http
  * Clase borracha para realizar las llamadas asincronas al API
  * http://developer.android.com/reference/android/content/AsyncTaskLoader.html
  * @author Daniel Serrano
+ * @param <D>
  *
  * @param <D> Tipo de dato que queremos devolver.
  */
-public class TareaAsincronaAPI<D> extends AsyncTaskLoader<D> {
+public class TareaAsincronaAPI<D> extends AsyncTaskLoader<List<D>> {
 
 	private String url;
 	private String jsonName;
@@ -38,44 +40,13 @@ public class TareaAsincronaAPI<D> extends AsyncTaskLoader<D> {
 	}
 
 	@Override
-	public D loadInBackground() {
+	public List<D> loadInBackground() {
         String response = sendRequest(url);
         return processResponse(response);
     }
 
 
     private String sendRequest(String url) {
-//        BufferedReader input = null; // get the json
-//        HttpURLConnection httpCon = null; // the http connection object
-//        StringBuilder response = new StringBuilder(); // hold all the data from the jason in string separated with "\n"
-//
-//        try {
-//            URL apiUrl = new URL(url);
-//            httpCon = (HttpURLConnection) apiUrl.openConnection();
-//
-//            if (httpCon.getResponseCode() != HttpURLConnection.HTTP_OK) { // check for connectivity with server
-//                return null;
-//            }
-//            input = new BufferedReader(new InputStreamReader(httpCon.getInputStream())); // pull all the json from the site
-//            String line;
-//            while ((line = input.readLine()) != null) {
-//                response.append(line + "\n");
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            if (input != null) {
-//                try {
-//                    input.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            if (httpCon != null) {
-//                httpCon.disconnect();
-//            }
-//        }
-//        return response.toString();
     	
 		HttpResponse response = null;
 		try {
@@ -94,22 +65,43 @@ public class TareaAsincronaAPI<D> extends AsyncTaskLoader<D> {
 		return (response != null) ? response.toString() : "";
     }
 
-    private D processResponse(String response) {
-        D deals = (D) new ArrayList<Object>();
+    @SuppressWarnings("unchecked")
+	private List<D> processResponse(String response) {
+        List<D> lista = new ArrayList<D>();
         
         if ( response != null ) {
 	        try {
 	            JSONObject responseObject = new JSONObject(response); // Creates a new JSONObject with name/value mappings from the JSON string.
 	            JSONArray results = responseObject.getJSONArray(this.jsonName); // Returns the value mapped by name if it exists and is a JSONArray.
 	            	
-	            for(int i = 0; i < results.length(); i++) {
-	            	JSONObject d = results.getJSONObject(i);
-	            }
+	            lista = (List<D>) parseJSON(results, jsonName);
 	        }catch (JSONException e) {
 	            e.printStackTrace();
 	        }
         }
         
-        return deals;
+        return lista;
+    }
+    
+    private Object parseJSON(JSONArray arrayJ, String nameJson) {
+    	
+    	List<Object> lista = new ArrayList<>();
+    	
+    	switch (nameJson) {
+		case "recetas":
+			JSONObject jo = null;
+			for ( int i = 0; i < arrayJ.length(); i++) {
+
+				try {
+					jo = arrayJ.getJSONObject(i);
+					lista.add(new Receta(jo.getString("nombre"), jo.getString("tipoplato"), jo.getString("ingredienteprincipal"), jo.getString("dificultad"), jo.getString("tiempo"), jo.getString("calorias"), jo.getBoolean("celiacos"), jo.getBoolean("lactosa"), jo.getString("ingredientes"), jo.getString("modo"), jo.getInt("puntuacion")));	
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+			break;
+		}
+    	
+    	return lista;
     }
 }
--- src/com/recetatordeveloperteam/recetator3000/framgmentos/BusquedaRecetasFragment.java
+++ src/com/recetatordeveloperteam/recetator3000/framgmentos/BusquedaRecetasFragment.java
@@ -3,22 +3,58 @@ package com.recetatordeveloperteam.recetator3000.framgmentos;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.Loader;
+import android.support.v4.content.Loader.OnLoadCompleteListener;
+import android.support.v4.view.ViewPager.LayoutParams;
 import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
+import com.recetatordeveloperteam.recetator3000.R;
 import com.recetatordeveloperteam.recetator3000.Receta;
 import com.recetatordeveloperteam.recetator3000.commandFactory.TareaAsincronaAPI;
 
 public class BusquedaRecetasFragment extends Fragment {
 	
-	private TareaAsincronaAPI<List<Receta>> tareaAsincrona;
+	private TareaAsincronaAPI<Receta> tareaAsincrona;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		tareaAsincrona = new TareaAsincronaAPI<List<Receta>>(getActivity().getApplication(), "http://recetator-api.appspot.com/getrecetas", "recetas");
+		final ListView listaView = (ListView) getActivity().findViewById(R.id.listaRecetas);
+		//listaView.addView(new LoadingView(getActivity().getApplication()));
+		
+		ProgressBar progressBar = new ProgressBar(getActivity());
+		LayoutParams params = new LayoutParams();
+		params.gravity = Gravity.CENTER;
+		params.height = LayoutParams.WRAP_CONTENT;
+		params.width = LayoutParams.WRAP_CONTENT;
+        progressBar.setLayoutParams(params);
+        progressBar.setIndeterminate(true);
+        listaView.setEmptyView(progressBar);
+		
+		tareaAsincrona = new TareaAsincronaAPI<Receta>(getActivity().getApplication(), "http://recetator-api.appspot.com/getrecetas", "recetas");
+		tareaAsincrona.registerListener(1, new OnLoadCompleteListener<List<Receta>>() {
+			@Override
+			public void onLoadComplete(Loader<List<Receta>> arg0, List<Receta> arg1) {
+				
+				listaView.removeAllViews();
+				for ( Receta r : arg1) {
+					listaView.addView(new RecetaView(getActivity().getApplication(), r));
+				}
+			}
+		});
 	}
 	
 	@Override
@@ -29,5 +65,114 @@ public class BusquedaRecetasFragment extends Fragment {
 	@Override
 	public void onDestroy() {
 		
+	}
+	
+	public class AdapterRecetas extends AdapterView<RecetaAdapter> {
+
+		public AdapterRecetas(Context context) {
+			super(context);
+		}
+
+		@Override
+		public RecetaAdapter getAdapter() {
+			return getAdapter();
+		}
+
+		@Override
+		public View getSelectedView() {
+			return new View(getContext());
+		}
+
+		@Override
+		public void setAdapter(RecetaAdapter arg0) {
+			setAdapter(arg0);
+		}
+
+		@Override
+		public void setSelection(int arg0) {
+			
+		}
+	}
+	
+	public class RecetaAdapter implements ListAdapter {
+
+		private List<Receta> lista;
+		
+		public RecetaAdapter(List<Receta> recetas ) {
+			lista = recetas;
+		}
+		
+		@Override
+		public int getCount() {
+			return lista.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return lista.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public int getItemViewType(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			return null;
+		}
+
+		@Override
+		public int getViewTypeCount() {
+			return 0;
+		}
+
+		@Override
+		public boolean hasStableIds() {
+			return true;
+		}
+
+		@Override
+		public boolean isEmpty() {
+			return lista.isEmpty();
+		}
+
+		@Override
+		public void registerDataSetObserver(DataSetObserver observer) {
+		}
+
+		@Override
+		public void unregisterDataSetObserver(DataSetObserver observer) {
+		}
+
+		@Override
+		public boolean areAllItemsEnabled() {
+			return false;
+		}
+
+		@Override
+		public boolean isEnabled(int arg0) {
+			return false;
+		}
+	}
+	
+	public class RecetaView extends View {
+
+		public RecetaView(Context context, Receta receta) {
+			super(context);
+		}
+	}
+	
+	public class LoadingView extends View {
+
+		public LoadingView(Context context) {
+			super(context);
+			
+		}
 	}
 }
